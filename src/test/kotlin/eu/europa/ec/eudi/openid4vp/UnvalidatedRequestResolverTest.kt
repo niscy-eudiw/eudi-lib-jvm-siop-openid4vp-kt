0205a7ecd449f72fc52582b88d0db7cae974b123
babisRoutis <haralampos.routis@netcompany.com>
2025-01-09 13:17:07 +0200

    Updated querysource checks

--- src/test/kotlin/eu/europa/ec/eudi/openid4vp/UnvalidatedRequestResolverTest.kt
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vp/UnvalidatedRequestResolverTest.kt
@@ -209,7 +209,7 @@ class UnvalidatedRequestResolverTest {
         suspend fun test(state: String? = null) {
             val authRequest =
                 "https://client.example.org/universal-link?" +
-                    "response_type=id_token vp_token" +
+                    "response_type=vp_token%20id_token" +
                     "&client_id=https%3A%2F%2Fclient.example.org%2Fcb" +
                     "&client_id_scheme=redirect_uri" +
                     "&redirect_uri=https%3A%2F%2Fclient.example.org%2Fcb" +
@@ -429,7 +429,6 @@ class UnvalidatedRequestResolverTest {
             claim("response_type", "vp_token")
             claim("nonce", "nonce")
             claim("response_mode", "direct_post")
-            claim("scope", "")
             claim("presentation_definition", Jackson.toJsonObject(presentationDefinition))
             claim("state", "638JwH0b2jrhGlAZQVa50KysVazkI-YpiFcLj2DLMalJpZK6XC22vAsPqXkpwAwXzfYpK-WLc3GhHYK8lbT6rw")
             claim("client_metadata", Jackson.toJsonObject(clientMetadata))
